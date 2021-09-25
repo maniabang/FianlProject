e6acdff58d1ae68818f727b22ffb60f935279716
maniabang <madmanno@naver.com>
2021-09-26 00:20:49 +0900

    ThirdDay

---
+++ gajaclone/src/App.js
@@ -0,0 +1,15 @@
+
+import Promotion from "./component/Promotion";
+import Slider from "./component/Slider";
+
+function App() {
+  return (
+    <div className="App">
+      <Promotion />
+      <Slider />
+    </div>
+
+  );
+}
+
+export default App;
