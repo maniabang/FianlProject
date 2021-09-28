73f89373ff918b05973f3e6288266a6948327f62
maniabang <madmanno@naver.com>
2021-09-28 16:48:06 +0900

    SixthDay

--- gaja/src/component/board/Board_main.js
+++ gaja/src/component/board/Board_main.js
@@ -9,19 +9,11 @@ export default function Board_main() {
             <div className="board">
                 <h1>Driver Review</h1>
                 <div className='form-wrapper'>
-<<<<<<< HEAD
-                    <input className="title-input" type='text' placeholder='Driver Name'/>
-                    <input className="title-input" type='text' placeholder='Title'/>
-                    <textarea className="text-area" placeholder='Content' />
-                </div>
-                <button className="btn_submit"><Link to="/board_submit" className="link2">SUBMIT</Link></button>
-=======
                     <input className="title-input" type='text' placeholder='Driver Id'/>
                     <input className="title-input" type='text' placeholder='Title'/>
                     <textarea className="text-area" placeholder='Content' />
                 </div>
                 <button className="submit-button"><Link to="/board_submit" className="link2">SUBMIT</Link></button>
->>>>>>> 50ea356d5bdcfff84355c46e450cc57c1720b30b
             </div>
         </div>
     );
