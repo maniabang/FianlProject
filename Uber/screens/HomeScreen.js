a8002e83b69b9148eea07ef97bd8538a941a2240
maniabang <madmanno@naver.com>
2021-10-04 11:08:26 +0900

    12thDay

--- Uber/screens/HomeScreen.js
+++ Uber/screens/HomeScreen.js
@@ -2,6 +2,10 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GOOGLE_MAPS_APIKEY } from '@env'; 
+
+
 
 const HomeScreen = () => {
     return (
@@ -16,6 +20,26 @@ const HomeScreen = () => {
                     source={{
                         uri: "https://links.papareact.com/gzs",
                     }}
+                />
+
+                <GooglePlacesAutocomplete
+                    placeholder="Where From" 
+                    styles={{
+                        container: {
+                            flex: 0,    
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en',
+                    }}
+
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
                 />
                 <NavOptions />
             </View>
